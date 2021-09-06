f6b441039d04a4cd049a0659896739ece88e8ddf
Joseph Willian <90014396+josephwillian@users.noreply.github.com>
2021-09-06 15:34:45 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -18,18 +18,20 @@ for (const link of links) {
 }
 
 /* mudar o header da página quando der scroll*/
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
-  if (this.window.scrollY >= navHeight) {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
     header.classList.add('scroll')
   } else {
+    // menor que a altura do header
     header.classList.remove('scroll')
   }
-})
+}
 
-/* Testimonials carousel slider swiper 
+/* Testimonials carousel slider swiper
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   pagination: {
@@ -52,8 +54,26 @@ scrollReveal.reveal(
   `#home .image, #home .text,
   #about .image, #about .text,
   #services header, #services .card,
-  #testimonials header, #testimonials .testimonials
-  #contact .text, #contact .links
+  #testimonials header, #testimonials .testimonials swiper-container
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   `,
   { interval: 100 }
 )
+
+/* Button back to top */
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/* When Scroll */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
